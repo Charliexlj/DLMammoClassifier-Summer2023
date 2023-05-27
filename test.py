58f4f72e87180fd9f4afc9d4435dd41ae0699ed1
Charliexlj <charliexlj@gmail.com>
2023-05-27 13:57:57 +0100

    test

--- test.py
+++ test.py
@@ -1,15 +1,7 @@
-import os
+import torch
+import torch_xla
+import torch_xla.core.xla_model as xm
 
-script_path = os.path.abspath(__file__)
-
-# Get the directory of the script
-script_directory = os.path.dirname(script_path)
-
-print(script_directory)
-
-# Specify the filename
-filename = "model.pth"
-
-# Combine the script directory and filename to get the full file path
-file_path = os.path.join(script_directory, f'model_{200}.pth')
-print(file_path)
+t = torch.randn(2, 2, device=xm.xla_device())
+print(t.device)
+print(t)
