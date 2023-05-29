3bb77c935c5dd473f0dceebd1342390c55fd91ea
Charliexlj <charliexlj@gmail.com>
2023-05-29 16:04:52 +0100

    ..

--- train/unet/encoder/train_encoder.py
+++ train/unet/encoder/train_encoder.py
@@ -56,7 +56,7 @@ def train_encoder(index, state_dict, dataset, lr=1e-3, pre_iter=0, niters=100,
         print("Process: {:1d}  |  Iter:{:4d}  |  Tr_loss: {:.4f}  |  Time: {}".format( # noqa
         index, it, loss, MMutils.convert_seconds_to_time(time.time()-start))) # noqa
 
-    MMutils.save_model(model, current_dir, n_iter)
+    MMutils.save_model(model, current_dir, niters)
 
 
 if __name__ == '__main__':
