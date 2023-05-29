06c9b69b00c7b56f10a6baa3da0a733811d70636
Charliexlj <charliexlj@gmail.com>
2023-05-29 20:27:00 +0100

    test

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import torch
+from Mammolibs import MMmodels
 
-state_dict = torch.load('./train/unet/encoder/model_iter_15.pth') # noqa
+en_state_dict = torch.load('./train/unet/encoder/model_iter_15.pth') # noqa
+model = MMmodels.Autoencoder()
 
-for name in state_dict.keys():
+for name in en_state_dict.keys():
+    print(name)
+
+for name in model.state_dict.keys():
     print(name)
