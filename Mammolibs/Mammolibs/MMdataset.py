02eea57cdf30c19bdc3186f479e12a304686599a
Charliexlj <charliexlj@gmail.com>
2023-06-04 21:30:00 +0200

    ..

--- Mammolibs/Mammolibs/MMdataset.py
+++ Mammolibs/Mammolibs/MMdataset.py
@@ -15,7 +15,7 @@ class MMImageSet(Dataset):
         super(MMImageSet, self).__init__()
         self.fs = gcsfs.GCSFileSystem()
         if aug: self.filenames = self.fs.ls(gcs_path)
-        else: self.filenames = [s for s in  self.fs.ls(gcs_path) if s.count('_') == 1]
+        else: self.filenames = [s for s in  self.fs.ls(gcs_path) if s.count('_') == 1 and s.endswith(('.png', '.jpg', '.jpeg'))]
         self.stage = stage
         if self.stage == 'finetune':
             self.labels = [filename.replace('CombinedBreastMammography', 'CombinedROIMask').replace('CBIS', 'CBIS_ROI') for filename in self.filenames] # noqa
