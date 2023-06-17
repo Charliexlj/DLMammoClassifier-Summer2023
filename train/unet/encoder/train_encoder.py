eaf8a307ce3a2daa6e68cbc137440b22b8160529
Charliexlj <charliexlj@gmail.com>
2023-06-17 17:43:05 +0100

    ..

--- train/unet/encoder/train_encoder.py
+++ train/unet/encoder/train_encoder.py
@@ -58,34 +58,34 @@ def train_encoder(index, state_dict, dataset, lr=1e-3, pre_iter=0, niters=10,
     loss = 100
     if index == 0:
         print('start main training loop...')
-        for it in range(pre_iter+1, pre_iter+niters+1):
-            para_train_loader = pl.ParallelLoader(train_loader, [device]).per_device_loader(device) # noqa
-            print('para_train_loader finished...')
-            start = time.time()
-            for batch_no, batch in enumerate(para_train_loader): # noqa
-                if index == 0:
-                    print(f'enter batch {batch_no}...')
-                images = batch
-                image0 = images[0].unsqueeze(0).repeat(batch_size, 1, 1, 1)
-                if index==0 and batch_no==0:
-                    print('images shape: ', images.shape)
-                    print('image0 shape: ', image0.shape)
-                images = torch.cat([image0, images], dim=0)
-                images = torch.stack([MMdataset.mutations(image) for image in images])
-                if index==0 and batch_no==0:
-                    print('combined images shape: ', images.shape)
-                
-                logits = model(images)
-                train_loss = criterion(logits, labels)
-                optimizer.zero_grad()
-                train_loss.backward()
-                xm.optimizer_step(optimizer)
-                loss = train_loss.cpu()
-                if test_flag:
-                    break
+    for it in range(pre_iter+1, pre_iter+niters+1):
+        para_train_loader = pl.ParallelLoader(train_loader, [device]).per_device_loader(device) # noqa
+        print('para_train_loader finished...')
+        start = time.time()
+        for batch_no, batch in enumerate(para_train_loader): # noqa
             if index == 0:
-                print("Master Process  |  Iter:{:4d}  |  Tr_loss: {:.4f}  |  Time: {}".format( # noqa
-                it, loss.item(), MMutils.convert_seconds_to_time(time.time()-start))) # noqa
+                print(f'enter batch {batch_no}...')
+            images = batch
+            image0 = images[0].unsqueeze(0).repeat(batch_size, 1, 1, 1)
+            if index==0 and batch_no==0:
+                print('images shape: ', images.shape)
+                print('image0 shape: ', image0.shape)
+            images = torch.cat([image0, images], dim=0)
+            images = torch.stack([MMdataset.mutations(image) for image in images])
+            if index==0 and batch_no==0:
+                print('combined images shape: ', images.shape)
+            
+            logits = model(images)
+            train_loss = criterion(logits, labels)
+            optimizer.zero_grad()
+            train_loss.backward()
+            xm.optimizer_step(optimizer)
+            loss = train_loss.cpu()
+            if test_flag:
+                break
+        if index == 0:
+            print("Master Process  |  Iter:{:4d}  |  Tr_loss: {:.4f}  |  Time: {}".format( # noqa
+            it, loss.item(), MMutils.convert_seconds_to_time(time.time()-start))) # noqa
 
     if index == 0:
         MMutils.save_model(model.cpu(), current_dir, pre_iter+niters)
@@ -109,7 +109,8 @@ if __name__ == '__main__':
         print(f'Find model weights at {current_dir}/model_iter_{pre_iter}.pth, loading...') # noqa
         print(f'Now start to train from iter {pre_iter}...')
 
-    gcs_path = 'gs://combined-dataset/unlabelled-dataset/CombinedBreastMammography/'
+    # gcs_path = 'gs://combined-dataset/unlabelled-dataset/CombinedBreastMammography/'
+    gcs_path = 'gs://unlabelled-dataset/BreastMammography256/'
     dataset = MMdataset.MMImageSet(gcs_path, aug=False)
 
     n_iter = 20
