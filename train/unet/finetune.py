0361a8bbd6850ee2f0c600122754c53947938593
Charliexlj <charliexlj@gmail.com>
2023-06-18 22:44:12 +0100

    ..

--- train/unet/finetune.py
+++ train/unet/finetune.py
@@ -58,6 +58,7 @@ def finetune(index, state_dict, dataset, lr=1e-3, pre_iter=0, niters=10,
         start = time.time()
         for batch_no, batch in enumerate(para_train_loader): # noqa
             batch_start = time.time()
+            images, labels = batch
             image_labels = torch.stack((images, labels), dim=1)
             image_labels = torch.stack([MMdataset.mutations(image_label) for image_label in image_labels])
             images = image_labels[:, 0, :, :, :]
