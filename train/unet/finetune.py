514d3c0469ef6469c62d6af703bb6f699c25dd54
Charliexlj <charliexlj@gmail.com>
2023-06-18 22:44:26 +0100

    ..

--- train/unet/finetune.py
+++ train/unet/finetune.py
@@ -113,7 +113,7 @@ if __name__ == '__main__':
         lr,             # lr
         pre_iter,       # pre_iter
         n_iter,         # niters
-        64,             # batch_size
+        128,             # batch_size
         current_dir     # current_dir
         ), start_method='forkserver')
     
