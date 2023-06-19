434bd98e7f8b46e6bf959bb07e73a2bfd894aaa3
Charliexlj <charliexlj@gmail.com>
2023-06-19 14:34:52 +0100

    ..

--- train/unet/finetune.py
+++ train/unet/finetune.py
@@ -56,6 +56,7 @@ def finetune(index, state_dict, dataset, lr=1e-3, pre_iter=0, niters=10,
         para_train_loader = pl.ParallelLoader(train_loader, [device]).per_device_loader(device) # noqa
         start = time.time()
         for batch_no, batch in enumerate(para_train_loader): # noqa
+            batch_start = time.time()
             images, labels = batch
             labels = labels.squeeze(1)
             '''
@@ -70,9 +71,14 @@ def finetune(index, state_dict, dataset, lr=1e-3, pre_iter=0, niters=10,
             train_loss.backward()
             xm.optimizer_step(optimizer)
             loss = train_loss.cpu()
+            if index==0 and batch_no%50==0:
+                print("Batch:{:4d}  |  Iter:{:4d}  |  Tr_loss: {:.4f}  |  Time: {}".format( # noqa
+                batch_no, it, loss, MMutils.convert_seconds_to_time(time.time()-batch_start))) # noqa
         if index == 0:
+            print("==============================================================================") # noqa
             print("Master Process  |  Iter:{:4d}  |  Tr_loss: {:.4f}  |  Time: {}".format( # noqa
             it, loss, MMutils.convert_seconds_to_time(time.time()-start))) # noqa
+            print("==============================================================================") # noqa
     if index == 0:
         MMutils.save_model(model.cpu(), current_dir, pre_iter+niters)
 
