d2d73996a8a43336005fe29c3a84d1f8b3d154ac
Charliexlj <charliexlj@gmail.com>
2023-06-22 21:09:52 +0100

    ..

--- Mammolibs/Mammolibs/MMdataset.py
+++ Mammolibs/Mammolibs/MMdataset.py
@@ -18,8 +18,6 @@ class MMImageSet(Dataset):
     def __init__(self, gcs_path, stage='encoder', aug=True):
         super(MMImageSet, self).__init__()
         self.fs = gcsfs.GCSFileSystem()
-        # if aug: self.filenames = [s for s in self.fs.ls(gcs_path) if s.endswith(('.png', '.jpg', '.jpeg'))] # noqa
-        # else: self.filenames = [s for s in self.fs.ls(gcs_path) if s.count('_') == 1 and s.endswith(('.png', '.jpg', '.jpeg'))] # noqa
         '''https://storage.cloud.google.com/last_dataset/labelled-dataset/BreastMammography/Benign/MDB_MAMMO_104_0_0.jpg'''
         self.stage = stage
         if self.stage == 'finetune':
@@ -29,7 +27,8 @@ class MMImageSet(Dataset):
             print(f'The dataset contain {len(self.filenames)} images...')
             self.labels = [filename.replace('BreastMammography', 'ROIMask').replace("_MAMMO_", "_ROI_", 1) for filename in self.filenames] # noqa
         else:
-            self.filenames = [s for s in self.fs.ls(gcs_path) if s.endswith(('.png', '.jpg', '.jpeg'))]
+            if aug: self.filenames = [s for s in self.fs.ls(gcs_path) if s.endswith(('.png', '.jpg', '.jpeg'))] # noqa
+            else: self.filenames = [s for s in self.fs.ls(gcs_path) if s.count('_') == 1 and s.endswith(('.png', '.jpg', '.jpeg'))] # noqa
         print(f'The dataset contain {len(self.filenames)} images...')
 
     def __len__(self):
