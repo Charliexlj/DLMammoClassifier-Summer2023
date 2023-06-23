5901f6dae684cae72166c0a8d9d4c2ca25f9bb2d
Charliexlj <charliexlj@gmail.com>
2023-06-23 18:10:27 +0100

    ..

--- train/unet/finetune.py
+++ train/unet/finetune.py
@@ -177,6 +177,7 @@ if __name__ == '__main__':
         print(f'Find model weights at {current_dir}/autoencoder/model_iter_{int(args.autoencoder)}.pth, loading...') # noqa
     elif args.pretrain == 'test':
         state_dict = None
+        pre_iter = 0
     else:
         pre_iter = int(args.pretrain)
         state_dict = torch.load(f'{current_dir}/model_iter_{pre_iter}.pth') # noqa
