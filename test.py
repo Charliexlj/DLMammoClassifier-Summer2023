fc7e9f7068992f01410ac857f3d7b7804d117c44
Charliexlj <charliexlj@gmail.com>
2023-06-23 18:25:28 +0100

    ..

--- test.py
+++ test.py
@@ -41,6 +41,13 @@ if __name__ == '__main__':
     # state_dict = torch.load(f'{current_dir}/train/unet/autoencoder/model_iter_{iter}.pth') # noqa
     # model.load_state_dict(state_dict)
     # print(f'Find model weights at {current_dir}/train/unet/autoencoder/model_iter_{iter}.pth, loading...') # noqa
+    
+    model = MMmodels.UNet()
+    iter = input("Model iter: ")
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    state_dict = torch.load(f'{current_dir}/train/unet/model_iter_{iter}.pth') # noqa
+    model.load_state_dict(state_dict)
+    print(f'Find model weights at {current_dir}/train/unet/model_iter_{iter}.pth, loading...') # noqa
 
     gcs_path = 'gs://last_dataset/labelled-dataset/BreastMammography/Benign/' # noqa
     gcs_path2 = gcs_path.replace('Benign', 'Malignant')
@@ -51,48 +58,58 @@ if __name__ == '__main__':
     labels_names = [filename.replace('BreastMammography', 'ROIMask').replace("MAMMO", "ROI", 1) for filename in filenames] # noqa
     print(f'The dataset contain {len(filenames)} images...')
 
-idx = [random.randint(0, len(labels_names)-1) for _ in range(36)]
+    idx = [random.randint(0, len(labels_names)-1) for _ in range(12)]
 
-images = read_images(filenames, idx)
-labels = read_images(labels_names, idx)
+    images = read_images(filenames, idx)
+    labels = read_images(labels_names, idx)
 
-images_np = images.numpy().reshape(36, 256, 256)*255
-labels_np = labels.numpy().reshape(36, 256, 256)
+    logits = model(images)
 
-# Create a blank array to hold the highlighted images
-highlighted_images = np.zeros((36, 256, 256, 3), dtype=np.uint8)
+    images_np = images.numpy().reshape(12, 256, 256)*255
+    labels_np = labels.numpy().reshape(12, 256, 256)
+    logits_np = torch.argmax(logits, dim=1).detach().numpy().reshape(12, 256, 256)
 
-for i in range(36):
-    original_image_gray = images_np[i]  # Grayscale original image
-    
-    # Convert the original grayscale image to three channels
-    original_image_rgb = cv2.cvtColor(original_image_gray, cv2.COLOR_GRAY2RGB)
-    
-    # Create a mask by thresholding the segmentation image
-    _, binary_mask = cv2.threshold(labels_np[i], 0, 255, cv2.THRESH_BINARY)
-    
-    # Convert the binary mask to a colored overlay (e.g., green)
-    overlay_color = (0, 255, 0)  # Green color
-    overlay = np.zeros_like(original_image_rgb, dtype=np.uint8)
-    overlay[np.where(binary_mask > 0)] = overlay_color
-    overlay = overlay.astype(original_image_rgb.dtype)
+    # Create a blank array to hold the highlighted images
+    highlighted_images = np.zeros((36, 256, 256, 3), dtype=np.uint8)
 
-    # Combine the overlay with the original image
-    highlighted_image = cv2.addWeighted(original_image_rgb, 0.9, overlay, 0.1, 0)
-    
-    # Store the highlighted image in the array
-    highlighted_images[i] = highlighted_image
+    for i in range(12):
+        original_image_gray = images_np[i]  # Grayscale original image
+        
+        # Convert the original grayscale image to three channels
+        original_image_rgb = cv2.cvtColor(original_image_gray, cv2.COLOR_GRAY2RGB)
+        
+        # Create a mask by thresholding the segmentation image
+        _, binary_mask_t = cv2.threshold(labels_np[i], 0, 255, cv2.THRESH_BINARY)
+        _, binary_mask_p = cv2.threshold(logits_np[i], 0, 255, cv2.THRESH_BINARY)
+        
+        # Convert the binary mask to a colored overlay (e.g., green)
+        overlay_color_t = (0, 255, 0)  # Green color
+        overlay_t = np.zeros_like(original_image_rgb, dtype=np.uint8)
+        overlay_t[np.where(binary_mask_t > 0)] = overlay_color_t
+        overlay_t = overlay_t.astype(original_image_rgb.dtype)
+        
+        overlay_color_p = (0, 0, 255)  # Blue color
+        overlay_p = np.zeros_like(original_image_rgb, dtype=np.uint8)
+        overlay_p[np.where(binary_mask_p > 0)] = overlay_color_p
+        overlay_p = overlay_p.astype(original_image_rgb.dtype)
 
-fig, axs = plt.subplots(6, 6, figsize=(24, 24))
+        # Combine the overlay with the original image
+        highlighted_image = cv2.addWeighted(original_image_rgb, 0.8, overlay_t, 0.1, 0)
+        highlighted_image_2 = cv2.addWeighted(highlighted_image, 0.8, overlay_p, 0.1, 0)
+        
+        # Store the highlighted image in the array
+        highlighted_images[i] = highlighted_image_2
 
-for i in range(6):
-    for j in range(6):
-        axs[i, j].imshow(highlighted_images[i*6+j])
-        axs[i, j].set_title(f'Image {i*6+j+1}')
-        axs[i, j].axis('off')
+    fig, axs = plt.subplots(3, 4, figsize=(18, 24))
 
-plt.tight_layout()
-plt.savefig('plot.png')
-plt.show()
+    for i in range(3):
+        for j in range(4):
+            axs[i, j].imshow(highlighted_images[i*3+j])
+            axs[i, j].set_title(f'Image {i*3+j+1}')
+            axs[i, j].axis('off')
 
-print('Saved plot.png')
+    plt.tight_layout()
+    plt.savefig('plot.png')
+    plt.show()
+
+    print('Saved plot.png')
