08f490a31270538a33a045ff24cf8e79309ef2d3
Charliexlj <charliexlj@gmail.com>
2023-06-24 23:58:49 +0100

    /\..

--- train/resnet/train_resnet.py
+++ train/resnet/train_resnet.py
@@ -60,7 +60,6 @@ def train_resnet(index, state_dict, dataset, lr=1e-3, pre_iter=0, niters=10, # n
     if os.path.exists(load_model):
         checkpoint=torch.load(load_model,map_location=torch.device('cpu'))
         model.load_state_dict(checkpoint['model_state'],strict=False)
-        optimizer.load_state_dict(checkpoint['optimizer_state'],strict=False)
         print("model loaded successfully")
         print('starting training after epoch: ',checkpoint['epoch'])
     model = model.to(device).train()
